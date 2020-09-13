08c1adf0571599430997895c050f1e424fed9008
Usman Ayupov <u.ayupov@dodopizza.com>
2020-09-13 12:53:51 +0300

    Add fields to get paged list for comments

--- Companies/Requests/CompanyCommentGetPagedListRequest.cs
+++ Companies/Requests/CompanyCommentGetPagedListRequest.cs
@@ -6,8 +6,14 @@ namespace Crm.V1.Clients.Companies.Requests
     {
         public Guid CompanyId { get; set; }
 
+        public DateTime? CommentGetPagedListRequest { get; set; }
+
         public DateTime? AfterCreateDateTime { get; set; }
 
         public int Limit { get; set; } = 10;
+
+        public string SortBy { get; set; } = "CreateDateTime";
+
+        public string OrderBy { get; set; } = "desc";
     }
 }
--- Contacts/Requests/ContactCommentGetPagedListRequest.cs
+++ Contacts/Requests/ContactCommentGetPagedListRequest.cs
@@ -6,8 +6,14 @@ namespace Crm.V1.Clients.Contacts.Requests
     {
         public Guid ContactId { get; set; }
 
+        public DateTime? CommentGetPagedListRequest { get; set; }
+
         public DateTime? AfterCreateDateTime { get; set; }
 
         public int Limit { get; set; } = 10;
+
+        public string SortBy { get; set; } = "CreateDateTime";
+
+        public string OrderBy { get; set; } = "desc";
     }
 }
